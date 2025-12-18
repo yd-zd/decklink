606926a1f71a8c272d72d53e8f465137a2a56109
Furkan Can Turan <furkancan.turan@nodos.dev>
2025-12-18 23:38:54 +0300

    Add required compile defs

---
+++ NosPluginCommon.cmake
@@ -0,0 +1,5 @@
+# Copyright MediaZ Teknoloji A.S. All Rights Reserved.
+
+function(nos_plugin_common current_dir out_target_dependencies out_target_definitions)
+    set(${out_target_definitions} "NOS_DISABLE_DEPRECATED" PARENT_SCOPE)
+endfunction()
