53e11333b73be944a515bc38550d4c49ca2dbddd
M. Şamil Ateşoğlu <samil.atesoglu@nodos.dev>
2026-03-03 14:53:33 +0300

    Show sync errors on node (#11)

--- nosDeckLink/Source/DeckLinkMain.cpp
+++ nosDeckLink/Source/DeckLinkMain.cpp
@@ -30,6 +30,8 @@ enum class Nodes : int
 	DMARead,
 	WaitFrame,
 	Channel,
+	Input,
+	Output,
 	Count
 };
 
@@ -41,6 +43,51 @@ nosResult RegisterChannelNode(nosNodeFunctions*);
 struct DeckLinkPluginFunctions : nos::PluginFunctions
 {
 	using PluginFunctions::PluginFunctions;
+
+	static nosResult MigrateInOutNodes(nosFbNodePtr node, nosBuffer* outBuffer)
+	{
+		auto pluginVersion = node->plugin_version();
+		auto isVersionLessThan = [](auto* version, int major, int minor) -> bool
+		{
+			if (!version)
+				return true;
+			if (version->major() != major)
+				return version->major() < major;
+			return version->minor() < minor;
+		};
+
+		bool needsNodeStatusPortalMigration = isVersionLessThan(pluginVersion, 1, 3);
+		if (!needsNodeStatusPortalMigration)
+			return NOS_RESULT_SUCCESS;
+
+		fb::TNode cur;
+		node->UnPackTo(&cur);
+
+		constexpr auto key = "NodeStatusPortal";
+		const bool isOutputNode = node->class_name() && node->class_name()->string_view().ends_with("decklink.Output");
+		const char* value = isOutputNode ? "/Auto Resize/ShowStatus;/Channel;/WaitFrame" : "/Channel;/WaitFrame";
+		bool migrated = false;
+
+		for (auto& metadata : cur.meta_data_map)
+		{
+			if (!metadata || metadata->key != key)
+				continue;
+			if (metadata->value != value)
+			{
+				metadata->value = value;
+				migrated = true;
+			}
+			break;
+		}
+
+		if (!migrated)
+			return NOS_RESULT_SUCCESS;
+
+		auto nodeBuffer = nos::EngineBuffer::CopyFrom(cur);
+		*outBuffer = nodeBuffer.Release();
+		return NOS_RESULT_SUCCESS;
+	}
+
 	nosResult ExportNodeFunctions(size_t& outSize, nosNodeFunctions** outList) override
 	{
 		outSize = static_cast<size_t>(Nodes::Count);
@@ -51,8 +98,18 @@ struct DeckLinkPluginFunctions : nos::PluginFunctions
 		NOS_RETURN_ON_FAILURE(RegisterWaitFrameNode(outList[(int)Nodes::WaitFrame]))
 		NOS_RETURN_ON_FAILURE(RegisterChannelNode(outList[(int)Nodes::Channel]))
 		NOS_RETURN_ON_FAILURE(RegisterDMAReadNode(outList[(int)Nodes::DMARead]))
+
+		// TODO: Remove these when migration of class-named graphs become available in Nodos.
+		*outList[(int)Nodes::Input] = nosNodeFunctions{
+			.ClassName = NOS_NAME("nos.decklink.Input"),
+			.MigrateNode = MigrateInOutNodes
+		};
+		*outList[(int)Nodes::Output] = nosNodeFunctions{
+			.ClassName = NOS_NAME("nos.decklink.Output"),
+			.MigrateNode = MigrateInOutNodes
+		};
 		return NOS_RESULT_SUCCESS;
 	}
 };
 NOS_EXPORT_PLUGIN_FUNCTIONS(DeckLinkPluginFunctions)
-} // namespace nos::aja
+} // namespace nos::decklink
