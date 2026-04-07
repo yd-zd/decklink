e768f910c895297c49758e9cabe04a397028b52e
M. Samil Atesoglu <samil.atesoglu@nodos.dev>
2026-04-07 18:11:20 +0300

    Fix DeckLink Studio 4K input configuration
    - Building possible configs now used in input scenarios too.
    - DeckLink subsystem has a new GetAvailableChannelConfigurations function to flatly query available configs.

--- nosDeckLinkSubsystem/Include/nosDeckLinkSubsystem/nosDeckLinkSubsystem.h
+++ nosDeckLinkSubsystem/Include/nosDeckLinkSubsystem/nosDeckLinkSubsystem.h
@@ -144,6 +144,27 @@ typedef struct nosDeckLinkChannelState {
 	uint64_t TimeInFrameNs;
 } nosDeckLinkChannelState;
 
+// For input, we have device, channel, and pixel format selection
+// For output, we have device, channel, pixel format, resolution, frame rate, and video scan type selection.
+typedef struct nosDeckLinkChannelConfig
+{
+	nosMediaIODirection Direction;
+	nosDeckLinkChannel Channel;
+	union {
+		struct
+		{
+			nosMediaIOPixelFormat PixelFormat;
+		} Input;
+		struct
+		{
+			nosMediaIOFrameGeometry Resolution;
+			nosMediaIOFrameRate FrameRate;
+			nosMediaIOVideoScanType ScanType;
+			nosMediaIOPixelFormat PixelFormat;
+		} Output;
+	};
+} nosDeckLinkChannelConfig;
+
 typedef void (NOSAPI_CALL* nosDeckLinkInputVideoFormatChangeCallback)(void* userData, nosMediaIOVideoScanType scanType, nosMediaIOFrameGeometry geometry, nosMediaIOFrameRate frameRate, nosMediaIOPixelFormat pixelFormat);
 typedef void (NOSAPI_CALL* nosDeckLinkFrameResultCallback)(void* userData, nosDeckLinkFrameResult result, uint32_t processedFrameNumber);
 typedef void (NOSAPI_CALL* nosDeckLinkDeviceInvalidatedCallback)(void* userData);
@@ -193,6 +214,12 @@ typedef struct nosDeckLinkSubsystem {
 
 	nosResult (NOSAPI_CALL* GetChannelState)(uint32_t deviceIndex, nosDeckLinkChannel channel, nosDeckLinkChannelState* outState);
 	nosResult (NOSAPI_CALL* ResetDropDetection)(uint32_t deviceIndex, nosDeckLinkChannel channel);
+
+	/// Returns all available channel configurations for a device.
+	/// Call with outConfigs=NULL to get the count in *inoutCount.
+	/// Call with outConfigs pointing to a buffer of *inoutCount elements to fill it.
+	/// Returns NOS_RESULT_INSUFFICIENT_BUFFER_SIZE if *inoutCount is too small.
+	nosResult (NOSAPI_CALL* GetAvailableChannelConfigurations)(uint32_t deviceIndex, nosDeckLinkChannelConfig* outConfigs, size_t* inoutCount);
 } nosDeckLinkSubsystem;
 
 #pragma region Helper Declarations & Macros
@@ -200,7 +227,7 @@ typedef struct nosDeckLinkSubsystem {
 // Make sure these are same with nossys file.
 #define NOS_DECKLINK_DEVICE_SUBSYSTEM_NAME "nos.sys.decklink"
 #define NOS_DECKLINK_DEVICE_SUBSYSTEM_VERSION_MAJOR 2
-#define NOS_DECKLINK_DEVICE_SUBSYSTEM_VERSION_MINOR 2
+#define NOS_DECKLINK_DEVICE_SUBSYSTEM_VERSION_MINOR 3
 
 extern struct nosModuleInfo nosDeckLinkSubsystemModuleInfo;
 extern nosDeckLinkSubsystem* nosDeckLink;
